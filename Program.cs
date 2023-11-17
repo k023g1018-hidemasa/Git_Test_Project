4b9b956106cbae0cec2cda47a66fc830f9bc941b
平島　秀正 <k023@1018@g.neec.ac.jp>
2023-11-17 12:05:39 +0900

    ローカルにコンフリクトをコミット

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, visyual studio git!");
-Console.WriteLine("github clone changed");
+Console.WriteLine("Confrict Changed!");
 //コミット忘れん事おｋ
