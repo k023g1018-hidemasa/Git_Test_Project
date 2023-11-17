e846f0ba0b7612b8a273f3fc18762863741ac185
平島　秀正 <k023@1018@g.neec.ac.jp>
2023-11-17 13:03:08 +0900

    コンフリクト対応

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, visyual studio git!");
-Console.WriteLine("Confrict Changed!");
+Console.WriteLine("Confrict Changed");
 //コミット忘れん事おｋ
