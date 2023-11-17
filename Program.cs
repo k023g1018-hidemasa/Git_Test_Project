db812dfb359b5865b723daa1ad0ff8ef97cd75c4
k023g1018-hidemasa <147122260+k023g1018-hidemasa@users.noreply.github.com>
2023-11-17 12:31:00 +0900

    ギっとを変更

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, visyual studio git!");
-Console.WriteLine("clone changed");
+Console.WriteLine("github clone changed");
 //コミット忘れん事おｋ
